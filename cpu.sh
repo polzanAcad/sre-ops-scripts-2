e792eef5d164597c454ce08da30b1b32051ad4df
polzanAcad <pj.abad@academy.opswerks.com>
2025-09-15 13:03:03 +0000

    task 1

---
+++ cpu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+while true
+do
+    # Get CPU idle percentage from top and subtract from 100
+    CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | \
+                awk '{print 100 - $8 "%"}')
+
+    # Print timestamp + CPU usage
+    #echo "$(date +"%Y-%m-%d %H:%M:%S") | CPU Usage: $CPU_USAGE"
+
+    echo "CPU usage: $CPU_USAGE"
+
+    #echo ""
+    #echo "Top 5 CPU-consuming processes:"
+    #ps -eo pid,comm,%cpu --sort=%cpu | head -n 6
+
+    #echo "Healthcheck run at: $(date)"
+
+    # Refresh every 1 second
+    sleep 1
+done
